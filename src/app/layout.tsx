583aea4afde5071fba02b47dba404697713187cd
Toon de Boer <toondeboer98@icloud.com>
2025-07-07 11:54:40 +0200

    add meta to head in html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
